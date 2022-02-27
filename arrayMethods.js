37bcd0e8254204405e7a2573f82b08bf081606b0
eduardo1199 <eduardo.soares.116@ufrn.edu.br>
2022-02-27 11:51:32 -0300

    treinando métodos para array

---
+++ arrayMethods.js
@@ -0,0 +1,14 @@
+const arrayNames = ['eduardo', 'guilherme', 'andreza', 'fabio'];
+
+const copyArrayNammes = [...arrayNames] //caso queira copiar os elementos para outro arrray
+
+const lengthOfArrayNames = arrayNames.length // tamanaho do array
+arrayNames.pop() //excluir último elemento
+arrayNames.shift() // remover primeiro array
+arrayNames.push('fabio') // adicionar elemento no fim
+arrayNames.unshift('eduardo') //adicionar elementos no começo do array
+const newArray = arrayNames.slice(1, 3) //repartir array do índice 1 ate 3 "['guilherme, andreza']"
+const newArrayCopy = copyArrayNammes.slice(0, -1) //repartir array do índice 1 ate 3 "['eduardo', 'guilherme', 'andreza']"
+
+const fromArray = 'Eduardo Soares de Araujo Aquino'.split(' '); //separar string em array
+const joinArray = arrayNames.join(' '); // juntar elementos do array em string
